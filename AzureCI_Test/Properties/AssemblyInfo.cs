8990a8b61821f96dc715c1fdb4b5e66c2f8146a5
Seigo Itou <seigo2t53@gmail.com>
2019-12-11 02:07:40 +0900

    プロジェクト ファイルを追加します。

---
+++ AzureCI_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AzureCI_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AzureCI_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7fd11cce-9903-43a0-986b-71fe6b62fc68")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AzureCI_Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AzureCI.App;
+using NUnit.Framework;
+
+namespace AzureCI_Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10,Adder.Add(6,4));
+        }
+    }
+}
