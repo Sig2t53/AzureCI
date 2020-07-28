c484bce610fcf1e82f091f439631fb89195b6b95
Seigo Itou <seigo2t53@gmail.com>
2020-07-28 21:56:09 +0900

    テスト

--- AzureCI.Application/Adder.cs
+++ AzureCI.Application/Adder.cs
@@ -12,5 +12,47 @@ namespace AzureCI.App
         {
             return a + b;
         }
+
+        public static void complexMethod()
+        {
+            for (int a = 0; a < 10; a++)
+            {
+                for (int b = 0; b < 10; b++)
+                {
+                    if (b == 0) { break; }
+                    for (int c = 0; c < 10; c++)
+                    {
+                        Console.WriteLine(c);
+                    }
+                }
+                if (a % 2 == 0)
+                {
+                    for (int d = a; d < 10; d++)
+                    {
+                        for (int e = 0; e < 10; e++)
+                        {
+                            if (d % 1 == 0) { Console.WriteLine(d); } else { Console.WriteLine(e); }
+                            if (d % 2 == 0) { Console.WriteLine(d); } else { Console.WriteLine(e); }
+                            if (d % 3 == 0) { Console.WriteLine(d); } else { Console.WriteLine(e); }
+                            if (d % 4 == 0) { Console.WriteLine(d); } else { Console.WriteLine(e); }
+                            if (d % 5 == 0) { Console.WriteLine(d); } else { Console.WriteLine(e); }
+                            if (d % 6 == 0) { Console.WriteLine(d); } else { Console.WriteLine(e); }
+                            if (d % 7 == 0) { Console.WriteLine(d); } else { Console.WriteLine(e); }
+                            if (d % 8 == 0) { Console.WriteLine(d); } else { Console.WriteLine(e); }
+                            if (d % 9 == 0) { Console.WriteLine(d); } else { Console.WriteLine(e); }
+                        }
+                        if (d % 1 == 0) { Console.WriteLine(d); }
+                        if (d % 2 == 0) { Console.WriteLine(d); }
+                        if (d % 3 == 0) { Console.WriteLine(d); }
+                        if (d % 4 == 0) { Console.WriteLine(d); }
+                        if (d % 5 == 0) { Console.WriteLine(d); }
+                        if (d % 6 == 0) { Console.WriteLine(d); }
+                        if (d % 7 == 0) { Console.WriteLine(d); }
+                        if (d % 8 == 0) { Console.WriteLine(d); }
+                        if (d % 9 == 0) { Console.WriteLine(d); }
+                    }
+                }
+            }
+        }
     }
 }
